ffd42300a8ffb768b3185e663d9cce3c1c8405e7
victor miguel <victormigueltejeda@gmail.com>
2023-11-21 11:08:52 -0500

    continun working

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Hero } from '../../interfaces/heros.interface';
+import { HerosService } from '../../service/hero.service';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-search-page',
@@ -8,4 +11,28 @@ import { FormControl } from '@angular/forms';
 })
 export class SearchPageComponent {
   public searchInput = new FormControl('');
+  public heroes: Hero[] = [];
+  public selectdHero?:Hero;
+
+  constructor( private heroesService:HerosService){}
+
+
+  searchHero(){
+    const value:string = this.searchInput.value || "";
+
+    this.heroesService.getSuggestions(value).subscribe(heroes => this.heroes =heroes)
+  }
+
+  onSelectedOption(event:MatAutocompleteSelectedEvent):void{
+
+    if(!event.option.value){
+      this.selectdHero = undefined;
+      return;
+    }
+
+    const hero:Hero = event.option.value;
+    this.searchInput.setValue(hero.superhero);
+
+    this.selectdHero = hero;
+  }
 }
--- src/app/heroes/service/hero.service.ts
+++ src/app/heroes/service/hero.service.ts
@@ -19,4 +19,9 @@ export class HerosService {
       .get<Hero>(`${this.baseUrl}/heroes/${id}`)
       .pipe(catchError((error) => of(undefined)));
   }
+
+  getSuggestions(query:string):Observable<Hero[]>{
+
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limit=6`)
+  }
 }
